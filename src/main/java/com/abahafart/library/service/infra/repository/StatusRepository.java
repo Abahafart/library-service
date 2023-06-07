e31e3597109a7cd39498284cae1aeedabd2e4d69
Alfredo Miranda  Flores <alfredo.miranda.learning@gmail.com>
2023-06-06 20:03:43 -0600

    add reactive repository

--- src/main/java/com/abahafart/library/service/infra/repository/StatusRepository.java
+++ src/main/java/com/abahafart/library/service/infra/repository/StatusRepository.java
@@ -1,9 +1,14 @@
 package com.abahafart.library.service.infra.repository;
 
 import com.abahafart.library.service.infra.repository.entity.StatusEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Mono;
 
 import java.util.UUID;
 
-public interface StatusRepository extends JpaRepository<StatusEntity, UUID> {
+public interface StatusRepository extends ReactiveCrudRepository<StatusEntity, UUID> {
+
+    @Query("SELECT CT.ID, CT.DESCRIPTION, CT.TYPE, CT.CREATED_DATE FROM library.cat_status CT WHERE CT.ID = ?")
+    Mono<StatusEntity> findById(UUID id);
 }
